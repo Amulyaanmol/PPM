801f46f0c765d9d77614a7508e3ec09844855d9c
Amulyaanmol <46672805+Amulyaanmol@users.noreply.github.com>
2021-09-20 19:36:26 +0530

    Add files via upload

---
+++ Model/Action/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Model.Action
+{
+    public class ActionResult
+    {
+        public bool IsPositiveResult {  get; set; }
+        public string Message {  get; set; }
+    }
+    public class DataResults<T> : ActionResult { public IEnumerable<T> results; }
+}
