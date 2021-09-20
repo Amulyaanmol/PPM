e6797ed4bb4161dce8bed8095c33eed4e8f2512f
Amulyaanmol <46672805+Amulyaanmol@users.noreply.github.com>
2021-09-20 19:36:55 +0530

    Add files via upload

---
+++ Doop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain;
+using Output;
+namespace PPM
+{
+    public class Program
+    {
+#pragma warning disable IDE0060 // Remove unused parameter
+        public static void Main(string[] args)
+#pragma warning restore IDE0060 // Remove unused parameter
+        {
+            Console.Clear();
+            int option1 = Display.DisplayMainMenu();
+            try
+            {
+                Display.MainCall(option1);
+                Console.Read();
+            }
+            catch (Exception)
+            {
+                    Console.WriteLine("please provide correct Input....");
+                    Display.MainCall(option1);
+                    Console.Read();
+                
+            }
+
+        }
+    }
+}
